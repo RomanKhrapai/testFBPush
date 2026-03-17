ca05364d278bcdd03e9efc9fcf0298fea4dfb1cc
Roman <hrapayr@gmail.com>
2026-03-17 17:52:47 +0200

    add message to list

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,13 @@
+const channel = new BroadcastChannel('fcm_notifications');
+
 self.addEventListener("push", (event) => {
     const data = event.data.json();
     console.log("Отримано push-повідомлення", data);
 
+    channel.postMessage(data);
+
     self.registration.showNotification(data.notification.title, {
         body: data.notification.body,
-        icon: "/icon.png", // Замініть шлях на ваш значок
+        icon: "/icon.png",
     });
 });
