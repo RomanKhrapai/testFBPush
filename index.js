d960c657ae4c4268a284f7890bf9aa767a2c21a1
Roman <hrapayr@gmail.com>
2026-03-17 14:11:30 +0200

    fix pash

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("firebase-messaging-sw.js")
+  // navigator.serviceWorker.register("/firebase-messaging-sw.js")
+  navigator.serviceWorker.register("testFBPush/firebase-messaging-sw.js")
     .then(registration => {
       console.log("Service Worker зареєстровано:", registration);
     })
