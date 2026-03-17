e0a9b6ce1d6369ce05834a51514ea200e8d0e0dc
Roman <hrapayr@gmail.com>
2026-03-17 14:27:05 +0200

    fix pash

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// if ("serviceWorker" in navigator) {
-//   // navigator.serviceWorker.register("/firebase-messaging-sw.js")
-//   navigator.serviceWorker.register("firebase-messaging-sw.js")
-//     .then(registration => {
-//       console.log("Service Worker зареєстровано:", registration);
-//     })
-//     .catch(error => {
-//       console.error("Помилка реєстрації Service Worker:", error);
-//       alert("Помилка реєстрації Service Worker:")
-//     });
-// }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/firebase-messaging-sw.js")
+    .then(registration => {
+      console.log("Service Worker зареєстровано:", registration);
+    })
+    .catch(error => {
+      console.error("Помилка реєстрації Service Worker:", error);
+      alert("Помилка реєстрації Service Worker:")
+    });
+}
