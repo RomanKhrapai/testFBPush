086842f84814af65d7e1f83fd52a42bdaf4e190e
Roman <hrapayr@gmail.com>
2026-03-17 18:05:50 +0200

    add channel

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,22 @@
 const channel = new BroadcastChannel('fcm_notifications');
 
 self.addEventListener("push", (event) => {
-    const data = event.data.json();
-    console.log("Отримано push-повідомлення", data);
+    let data = {};
+    try {
+        data = event.data.json();
+    } catch (e) {
+        data = { notification: { title: "Тестовий пуш", body: event.data.text() } };
+    }
+
+    console.log("SW: Отримано push", data);
 
     channel.postMessage(data);
 
-    self.registration.showNotification(data.notification.title, {
-        body: data.notification.body,
+    const title = data.notification?.title || "Нове повідомлення";
+    const options = {
+        body: data.notification?.body || "",
         icon: "/icon.png",
-    });
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
 });
